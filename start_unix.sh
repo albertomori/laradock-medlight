3f0a5c0796e9c8b36b9de74f00f311f21c45228a
Al <alberto.mori@surfacesrl.com>
2023-08-02 11:23:00 +0200

    Invio e stampa

--- start_unix.sh
+++ start_unix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#sudo service nginx stop
-#sudo service redis-server stop
-#mysqladmin -u root -p shutdown
+# sudo service nginx stop
+# sudo service redis-server stop
+# mysqladmin -u root -p shutdown
 
 docker compose up -d nginx mysql mongo phpmyadmin redis workspace
